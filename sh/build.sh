f6d41cf7e27176bda10f76e2821b11278a37dbef
Patrick <15920996910sun@gmail.com>
2016-10-12 18:12:42 +0800

    更换了 dashboard 模板

--- sh/build.sh
+++ sh/build.sh
@@ -2,19 +2,30 @@
 
 cd `dirname $0`
 
-echo '>> Prepare express'
+echo "\033[34m ======== Prepare express ======== \033[0m"
 cd ..
 npm install
 
 #######################
 
-echo '>> Prepare app'
+echo "\033[34m ======== Prepare app ======== \033[0m"
 cd app
 npm install
 bower install
 
-echo '>> Start gulp defalut task'
+echo "\033[34m ======== Start gulp defalut task ======== \033[0m"
 gulp build
 
+#######################
 
-echo '>> Build shell complate'
+cd `dirname $0`
+
+echo "\033[34m ======== Prepare dashboard ======== \033[0m"
+cd dashboard
+npm install
+
+gem install sass
+
+gulp build
+
+echo "\033[34m ======== Build shell complate ======== \033[0m"
